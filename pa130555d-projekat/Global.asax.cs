a840625aa2c117869961481a99aeae693b05f66f
Ana <Ana@ANA>
2016-08-10 16:16:33 +0200

    fixed bug

--- pa130555d-projekat/Global.asax.cs
+++ pa130555d-projekat/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using pa130555d_projekat.Models;
 
 namespace pa130555d_projekat
 {
@@ -16,6 +18,7 @@ namespace pa130555d_projekat
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<ApplicationDbContext>(null);
         }
     }
 }
